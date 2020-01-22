6a9492226e55bd8e484dcd171df28efbe5fcbee2
Dani <dani.93.perez@gmail.com>
2020-01-22 18:44:52 +0000

    add Spinner

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     //Same as this.state = {lat: null}; in constructor
-    state = {late: null};
+    state = {lat: null};
 
     componentDidMount() {
         this.setState({lat: -37.77});
     }
 
+    renderContent() {
+        if (this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        
+        return <Spinner text="Sending some"/>
+    }
+
     render() {
-    return <SeasonDisplay lat={this.state.lat}/>
+        return (
+            <div className="example">{this.renderContent()}</div>
+        )
     }
 }
 
